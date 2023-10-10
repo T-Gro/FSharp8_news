b1ded57384074dc87efd5b88effbe80c30078d0a
Tomas Grosup <tomasgrosup@microsoft.com>
2023-10-10 16:54:09 +0200

    tailcall attr example added

--- FSharp8_WhatsNew/ArithmeticLiterals.fs
+++ FSharp8_WhatsNew/ArithmeticLiterals.fs
@@ -3,6 +3,10 @@
 let [<Literal>] bytesInKB = 2f ** 10f
 let [<Literal>] bytesInMB = bytesInKB * bytesInKB
 let [<Literal>] bytesInGB = 1 <<< 30
-let [<Literal>] customBitMask = 0b00000101uy
-let [<Literal>] inverseBitMasp = ~~~ customBitMask
+let [<Literal>] customBitMask = 0b01010101uy
+let [<Literal>] inverseBitMask = ~~~ customBitMask
 
+type MyEnum = 
+    | A = (1 <<< 5)
+    | B = (17 * 45 % 13)
+    | C = bytesInGB
---
+++ FSharp8_WhatsNew/TailCallCheck.fs
@@ -0,0 +1,9 @@
+﻿module TailCallCheck
+
+let mul x y = x * y
+
+[<TailCall>]
+let rec fact n acc =
+    if n = 0
+    then acc
+    else (fact (n - 1) (mul n acc)) + 23

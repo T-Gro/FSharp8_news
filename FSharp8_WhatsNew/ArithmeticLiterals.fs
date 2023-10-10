2b1a20c9a5a37cbbb070b3bebb2a48b085d195dd
Tomas Grosup <tomasgrosup@microsoft.com>
2023-10-10 15:33:49 +0200

    literals

---
+++ FSharp8_WhatsNew/ArithmeticLiterals.fs
@@ -0,0 +1,8 @@
+﻿module ArithmeticLiterals
+
+let [<Literal>] bytesInKB = 2f ** 10f
+let [<Literal>] bytesInMB = bytesInKB * bytesInKB
+let [<Literal>] bytesInGB = 1 <<< 30
+let [<Literal>] customBitMask = 0b00000101uy
+let [<Literal>] inverseBitMasp = ~~~ customBitMask
+
